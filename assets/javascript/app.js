b7e57604152009e41a85e75d372dbc0c5f52e45e
chandni <chandnibpatel@gmail.com>
2019-01-18 19:14:55 -0600

    added audio and play again functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//obeject defination of questions
 var q1 ={
     qId:"q1",
     qText:"The value of (0.625 * 0.0729 * 28.9)/(0.0017 * 0.025 * 8.1) is",
@@ -60,8 +61,17 @@ var q10 ={
     options: ["150", "200", "225","300"],
     correctAns: "300"
 }
-var queArr = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
-var noOfQueToDisp = 8;
+var q11 ={   
+    qId:"q11",
+    qText:"The life time of a dog and his owner was 96 years in total. The owner lived 3 times longer than his dog, how many years did the owner live ?",
+    options: ["24", "34", "58","72"],
+    correctAns: "72"
+}
+//Array of question objects
+var queArr = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11];
+
+//variable declarations
+var noOfQueToDisp = 10;
 var loadedQue = [];
 var remTime = 10;
 var intervalId;
@@ -70,7 +80,14 @@ var currentQue=0;
 var correctAnsCount=0;
 var incorrectAnsCount=0;
 var unansweredCount=0;
+var moneyEarned =0;
 
+// audio files
+var audioKBC = new Audio("./assets/sounds/KBC_Theme.wav");
+var audioKBCwrong = new Audio("./assets/sounds/Wrong2.mp3");
+var audioKBCtimeOut = new Audio("./assets/sounds/timeout.mp3");
+
+//Function to start the Timer
 function startTimer(){
      remTime = 10;
        //start timer
@@ -80,6 +97,7 @@ function startTimer(){
     } 
 }
 
+//count function to decrement the timer 
 function count() {
     //decrement time by 1
     if(remTime > 0){
@@ -109,10 +127,10 @@ function loadQuePage(){
        
     }
 }
-
+//Function to display the loaded questions
 function displayQue(){
     var newDiv;
-    $("#questionDiv").show();
+    $("#optionGrp").show();
     $("#resultSummaryDiv").hide();
     $("#resultDiv").hide();
     
@@ -128,7 +146,6 @@ function displayQue(){
            var objName=
         optionDivObj=$("#option"+j);
         optionDivObj.empty();
-       // optionDivObj.text=loadedQue[currentQue].options[j]
         optionDivObj.append(loadedQue[currentQue].options[j]);
        }
 
@@ -136,55 +153,67 @@ function displayQue(){
       startTimer();
     } else{
             $("#questionDiv").hide();
-            $("#questionDiv").hide();
             $("#resultSummaryDiv").show();
             resultSummary();
     }
 }
 
+//display the result message for every questions 
 function displayResult(resultText){
     var qAns ;
     var imgUrl;
     
-    // $("#questionDiv").hide();
+     $("#optionGrp").hide();
     $("#resultSummaryDiv").hide();
     if (resultText==="Correct"){
         imgUrl="./assets/images/correct.gif";
         
-        $("#resultlbl").text('You are correct!!...');}
+        $("#resultlbl").text('You are correct!!..You have earned $' + moneyEarned);
+        audioKBC.play();}
     else if(resultText==="Incorrect")
        { 
         imgUrl="./assets/images/wrong.gif";
-        $("#resultlbl").text("Sorry ! Correct answer is " + rightAnswer );
+        $("#resultlbl").text("Sorry ! Correct answer is " + rightAnswer + " Your earned balance is $"+ moneyEarned );
+        audioKBCwrong.play();
     }
     else {
         imgUrl="./assets/images/timeout.gif";
-        $("#resultlbl").text("Timeout  ! Correct answer is " + rightAnswer );
+        audioKBCtimeOut.play();
+        $("#resultlbl").text("Timeout  ! Correct answer is " + rightAnswer + " Your earned balance is $"+ moneyEarned  );
+      
     }
     $("#resultImg").attr("src",imgUrl);
     $("#resultDiv").show();
+    
     var windowTimeout =setTimeout(function() {
         currentQue+=1;
+        $("#resultImg").attr("src","");
        displayQue();
-      }, 5000);
+      }, 6000);
  }
 
-
+//Result summary function to display the results at the end of the game
 function resultSummary(){
+    console.log("moneyEarned:" + moneyEarned);
+    if (moneyEarned === 10000000){
+        $("#gameResults").text("Congratulations! You have become a 'Crorepati', Earned: $" + moneyEarned );
+    }else{
+        $("#gameResults").text("All Done! Your Earned: $" + moneyEarned );
+    }
+ 
     $("#correctAnsCnt").text("Correct Answers: "+ correctAnsCount);
     $("#incorrectAnsCnt").text("Incorrect Answers: "+ incorrectAnsCount);
     $("#unansweredCnt").text("Unanswered: "+ unansweredCount);
 }
-function disableButton(){
 
-}
+//user button click function checks the user answer
 function buttonClick(selectedText){
-    $("#questionDiv").attr("disabled","disabled");
     clearInterval(intervalId);
     if (rightAnswer===selectedText)
     {
         correctAnsCount++;
         resultText="Correct"
+        moneyEarned = moneyEarned + 1000000;
     }
     else
     {
@@ -192,18 +221,34 @@ function buttonClick(selectedText){
         resultText="Incorrect"
     }
     displayResult(resultText);
-   
-
 }
-//$('input[name=gender]:checked').val()
+
+
+// Main Process
 $(document).ready(function() {
-    $("#questionDiv").on("click",".options", function(){
-        var selectedText=$(this).text();
-        console.log("This " + selectedText);
-        buttonClick(selectedText);
-    });
 
         loadQuePage();
-        displayQue();
-      
+        displayQue();      
+});
+
+//User click on options to choose an answer
+$("#questionDiv").on("click",".options", function(){
+    var selectedText=$(this).text();
+    buttonClick(selectedText);
+});
+
+//Play Again button click to restart the game
+$("#playAgainBtn").on("click", function(){
+    $("#resultSummaryDiv").hide();
+    $("#questionDiv").show();
+    loadedQue = [];
+    rightAnswer="";
+    currentQue=0;
+    correctAnsCount=0;
+    incorrectAnsCount=0;
+    unansweredCount=0;
+    moneyEarned = 0;
+    
+    loadQuePage();
+    displayQue();
 });
